401a235d2609556e74031466b84c37ec5dc00a1e
Holger Huo <holgerhuo@outlook.com>
2021-01-01 23:46:30 +0800

    Initial Commit

---
+++ search.php
@@ -0,0 +1,76 @@
+<?php
+// 你的 API 服务器地址
+define("API_URL", "https://cdn.zerodream.net/netease");
+if(isset($_GET['s']) && !empty($_GET['s'])) {
+	$keyWord = urlencode($_GET['s']);
+	$rawdata = @file_get_contents(API_URL . "/api.php?source=netease&types=search&name={$keyWord}&count=10&pages=1");
+	$data = json_decode($rawdata, true);
+	if(!$data || empty($data)) {
+		if(isset($_GET['debug'])) {
+			exit($rawdata);
+		}
+		exit("<center><p>未找到结果</p></center>");
+	}
+} else {
+	exit("<center><p>请在输入框中输入歌名后搜索</p></center>");
+}
+function getArtists($data) {
+	if(count($data) > 1) {
+		$artists = "";
+		foreach($data as $artist) {
+			$artists .= $artist . ",";
+		}
+		$artists = $artists == "" ? "未知歌手" : mb_substr($artists, 0, mb_strlen($artists) - 1);
+	} else {
+		$artists = $data[0];
+	}
+	return $artists;
+}
+?>
+<html>
+	<head>
+		<meta name="theme-color" content="#009688" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=11">
+		<title>669点歌台</title>
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/materialize-css@1.0.0/dist/css/materialize.min.css">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
+		<style>.table tr{font-size:14px;}.table .result:hover{cursor:pointer;color:#009688 !important;}.table tr th,.table tr td{white-space: nowrap;}</style>
+	</head>
+	<body style="display: none;">
+		<table class="table" id="musicList">
+			<tr>
+				<th>歌名</th>
+				<th>歌手</th>
+				<th>专辑</th>
+			</tr>
+			<?php
+			foreach($data as $music) {
+				echo "<tr class='result' onclick='select({$music['id']})'>
+				<td>{$music['name']}</td>
+				<td>" . getArtists($music['artist']) . "</td>
+				<td>{$music['album']}</td>
+			</tr>";
+			}
+			?>
+		</table>
+	</body>
+	<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery@3.5.0/dist/jquery.min.js"></script>
+	<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/materialize-css@1.0.0/dist/js/materialize.min.js"></script>
+	<script type="text/javascript">
+	function select(data) {
+		try {
+			window.parent.msginput.value = "点歌 " + data;
+			window.parent.$(window.parent.search).fadeOut();
+			window.parent.$(window.parent.msginput).focus();
+		} catch(e) {
+			// No
+		}
+	}
+	window.onload = function() {
+		$(document.body).fadeIn();
+	}
+	</script>
+</html>
---
+++ sw.js
@@ -0,0 +1,46 @@
+"use strict";
+var workboxVersion = "5.1.3";
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/".concat(workboxVersion, "/workbox-sw.js")),
+    workbox.core.setCacheNameDetails({ prefix: "radio_669_diange" }),
+    workbox.core.skipWaiting(),
+    workbox.core.clientsClaim(),
+    workbox.precaching.precacheAndRoute([
+        { revision: "njkfdbshk26748923hjksdvgs36843bh", url: "/index.html" },
+        { revision: "66646d5031b167545fbdbfd8d28ae3bf", url: "/static/css/style.css" },
+        { revision: "bd9588cf39d38517a31f1q8qqe470aff", url: "/static/css/night.css" },
+        { revision: "59323e91008ce69se66ce07371f6c84d", url: "/static/js/app.js" },
+        { revision: "89e0f78ebc6426c8ca88ce0a5dc86a1a", url: "/static/js/theme_auto.js" },
+        { revision: "7770c57744ed390f8fe9b61214d83b12", url: "/static/images/avatar.png" },
+        { revision: "abbf7d07aac53217336f8bae8d789265", url: "/static/images/apple-touch-icon.png" },
+        { revision: "a027656d01b1c4f1aba3217077201d96", url: "/static/images/icon-512x512.png" },
+        { revision: "76c320fe79e8ea43468b77a66c872183", url: "/static/images/icon-128x128.png" },
+        { revision: "f3654d7e814d32915db082787a419a2e", url: "/static/images/icon-72x72.png" },
+    ]),
+    workbox.precaching.cleanupOutdatedCaches(),
+    workbox.routing.registerRoute(
+        /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)$/,
+        new workbox.strategies.CacheFirst({
+            cacheName: "images",
+            plugins: [new workbox.expiration.ExpirationPlugin({ maxEntries: 1e3, maxAgeSeconds: 2592e3 }), new workbox.cacheableResponse.CacheableResponsePlugin({ statuses: [0, 200] })],
+        })
+    ),
+    workbox.routing.registerRoute(
+        /\.(?:eot|ttf|woff|woff2)$/,
+        new workbox.strategies.CacheFirst({ cacheName: "fonts", plugins: [new workbox.expiration.ExpirationPlugin({ maxEntries: 1e3, maxAgeSeconds: 2592e3 }), new workbox.cacheableResponse.CacheableResponsePlugin({ statuses: [0, 200] })] })
+    ),
+    workbox.routing.registerRoute(/^https:\/\/fonts\.googleapis\.com/, new workbox.strategies.StaleWhileRevalidate({ cacheName: "google-fonts-stylesheets" })),
+    workbox.routing.registerRoute(
+        /^https:\/\/fonts\.gstatic\.com/,
+        new workbox.strategies.CacheFirst({
+            cacheName: "google-fonts-webfonts",
+            plugins: [new workbox.expiration.ExpirationPlugin({ maxEntries: 1e3, maxAgeSeconds: 2592e3 }), new workbox.cacheableResponse.CacheableResponsePlugin({ statuses: [0, 200] })],
+        })
+    ),
+    workbox.routing.registerRoute(
+        /^https:\/\/cdn\.jsdelivr\.net/,
+        new workbox.strategies.CacheFirst({
+            cacheName: "static-libs",
+            plugins: [new workbox.expiration.ExpirationPlugin({ maxEntries: 1e3, maxAgeSeconds: 2592e3 }), new workbox.cacheableResponse.CacheableResponsePlugin({ statuses: [0, 200] })],
+        })
+    ),
+    workbox.googleAnalytics.initialize();
